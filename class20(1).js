c4300970d2ddfd91603ed8de7c25e8ca97b344ed
AaryaSandesh <aaryasandesh2601@gamil.com>
2025-03-11 14:45:32 +0530

    Conditionals

---
+++ class20(1).js
@@ -0,0 +1,20 @@
+// Conditionals 
+// if-else
+// switch
+let age=53
+if(age>=18){
+    console.log("can vote")
+}
+else{
+    console.log("cannot vote")
+}
+let number=3
+switch(num){
+    case 1:console.log('a')
+    break
+    case 2:console.log('b')
+    break
+    case 3:console.log('c')
+    break
+    default :console.log('d')
+}
